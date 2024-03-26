77164fc5680318e4641aab8a9943d5487ff84278
sethlxk <60959340+sethlxk@users.noreply.github.com>
2024-03-26 11:14:59 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+import google.generativeai as palm
+palm.configure(api_key="AIzaSyC-Ss6rUvCZNAxG5saZBcwzy3TO66BlNXU")
+model = {'model': "models/chat-bison-001"}
+app = Flask(__name__)
+
+@app.route("/",methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
+
+@app.route("/main",methods=["GET", "POST"])
+def main():
+    name = request.form.get("name")
+    return (render_template("main.html", r= name))
+
+@app.route("/palm_request",methods=["GET", "POST"])
+def palm_request():
+    return (render_template("palm.html"))
+
+@app.route("/palm_reply",methods=["GET", "POST"])
+def palm_reply():
+    q = request.form.get("q")
+    r = palm.chat(**model, messages=q)
+    return (render_template("palm_reply.html", r=r.last))
+
+if __name__ == "__main__":
+    app.run()
+    
